4bfe532c75578eb0c91a75f0cba8f149aa5cccd0
Mario Luzeiro <mrluzeiro@ua.pt>
2024-05-27 23:26:20 +0100

    test: add more tests to rcobs

--- dzrcobs/test/rcobs/test_rcobs.cpp
+++ dzrcobs/test/rcobs/test_rcobs.cpp
@@ -19,6 +19,8 @@
 #include <CppUTest/TestHarness.h>
 #include <cstddef>
 #include <cstdint>
+#include <cstdio>
+#include <cstdlib>
 #include <dzrcobs/rcobs.h>
 #include "CppUTest/UtestMacros.h"
 
@@ -27,7 +29,7 @@
 
 #define UTEST_GUARD_BYTE ( 0xEE )
 #define UTEST_GUARD_SIZE ( 4 )
-#define UTEST_ENCODED_DECODED_DATA_MAX_SIZE ( 256 + 1 )
+#define UTEST_ENCODED_DECODED_DATA_MAX_SIZE ( 1024 )
 
 // Setup
 // /////////////////////////////////////////////////////////////////////////////
@@ -245,6 +247,99 @@ TEST( RCOBS, EncodeEndInvalidArgs )
 
 	ret = rcobs_encode_inc_end( NULL, &sizeEncoded );
 	CHECK_EQUAL_TEXT( RCOBS_RET_ERR_BAD_ARG, ret, "NULL input must fail" );
+}
+
+TEST( RCOBS, EncodeOverflow )
+{
+	eRCOBS_ret ret = RCOBS_RET_SUCCESS;
+	sRCOBS_ctx ctx;
+	size_t sizeEncoded = 0;
+
+	ret = rcobs_encode_inc_begin( &ctx, buffer + UTEST_GUARD_SIZE, 2 );
+	CHECK_EQUAL( RCOBS_RET_SUCCESS, ret );
+
+	uint8_t testBuffer[2] = { 0 };
+
+	ret = rcobs_encode_inc( &ctx, testBuffer, 2 );
+	CHECK_EQUAL( RCOBS_RET_ERR_OVERFLOW, ret );
+}
+
+TEST( RCOBS, EncodeFirstOKThenOverflow )
+{
+	eRCOBS_ret ret = RCOBS_RET_SUCCESS;
+	sRCOBS_ctx ctx;
+	size_t sizeEncoded = 0;
+
+	ret = rcobs_encode_inc_begin( &ctx, buffer + UTEST_GUARD_SIZE, 2 );
+	CHECK_EQUAL( RCOBS_RET_SUCCESS, ret );
+
+	uint8_t testBuffer[2] = { 0 };
+
+	ret = rcobs_encode_inc( &ctx, testBuffer, 1 );
+	CHECK_EQUAL( RCOBS_RET_SUCCESS, ret );
+
+	ret = rcobs_encode_inc( &ctx, testBuffer, 1 );
+	CHECK_EQUAL( RCOBS_RET_ERR_OVERFLOW, ret );
+}
+
+TEST( RCOBS, EncodeLongBuffer )
+{
+	eRCOBS_ret ret = RCOBS_RET_SUCCESS;
+	sRCOBS_ctx ctx;
+	size_t sizeEncoded = 0;
+
+	static const uint32_t guard = ( UTEST_GUARD_BYTE << 24 ) | ( UTEST_GUARD_BYTE << 16 ) | ( UTEST_GUARD_BYTE << 8 ) |
+																( UTEST_GUARD_BYTE << 0 );
+
+	memset( buffer, UTEST_GUARD_BYTE, UTEST_ENCODED_DECODED_DATA_MAX_SIZE + UTEST_GUARD_SIZE * 2 );
+
+	ret = rcobs_encode_inc_begin( &ctx, buffer + UTEST_GUARD_SIZE, UTEST_ENCODED_DECODED_DATA_MAX_SIZE );
+	CHECK_EQUAL( RCOBS_RET_SUCCESS, ret );
+
+	const size_t sizeToEncode = 512;
+
+	uint8_t *decodedData = new uint8_t[UTEST_GUARD_SIZE + sizeToEncode + UTEST_GUARD_SIZE];
+
+	for( size_t i = 0; i < sizeToEncode; i++ )
+	{
+		decodedData[i] = rand();
+	}
+
+	ret = rcobs_encode_inc( &ctx, decodedData + ( sizeToEncode / 2 ) * 0, sizeToEncode / 2 );
+	CHECK_EQUAL( RCOBS_RET_SUCCESS, ret );
+
+	ret = rcobs_encode_inc( &ctx, decodedData + ( sizeToEncode / 2 ) * 1, sizeToEncode / 2 );
+	CHECK_EQUAL( RCOBS_RET_SUCCESS, ret );
+
+	ret = rcobs_encode_inc_end( &ctx, &sizeEncoded );
+	CHECK_EQUAL( RCOBS_RET_SUCCESS, ret );
+
+	CHECK_EQUAL( 0, memcmp( buffer, &guard, UTEST_GUARD_SIZE ) );
+	CHECK_EQUAL( 0, memcmp( buffer + UTEST_GUARD_SIZE + UTEST_ENCODED_DECODED_DATA_MAX_SIZE, &guard, UTEST_GUARD_SIZE ) );
+
+	uint8_t *decodedBuffer = new uint8_t[UTEST_GUARD_SIZE + ( sizeToEncode + 1 ) + UTEST_GUARD_SIZE];
+	uint8_t *decodedBufferPos;
+
+	size_t sizeDecoded = 0;
+	ret =
+	 rcobs_decode( buffer + UTEST_GUARD_SIZE, sizeEncoded, decodedBuffer, sizeToEncode, &sizeDecoded, &decodedBufferPos );
+	CHECK_EQUAL( RCOBS_RET_SUCCESS, ret );
+
+	CHECK_EQUAL( sizeToEncode, sizeDecoded );
+	CHECK_EQUAL( decodedBuffer, decodedBufferPos );
+
+	for( size_t i = 0; i < sizeToEncode; i++ )
+	{
+		if( decodedData[i] != decodedBufferPos[i] )
+		{
+			printf( "Failed at %lu, 0x%02X != 0x%02X\n", i, decodedData[i], decodedBufferPos[i] );
+			break;
+		}
+	}
+	CHECK_EQUAL( 0, memcmp( decodedData, decodedBufferPos, sizeDecoded ) );
+
+	delete[] decodedBuffer;
+	delete[] decodedData;
 }
 
 // EOF
